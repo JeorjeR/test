6eda0e7337a1b87ec99c69d20e22b5909d5c7c9b
Egor Roganin <eroganin@bk.ru>
2022-09-10 10:58:20 +0300

    Изменение строки Hello World -> Hello World!1

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 	System.out.println("1");
-	System.out.println("Hello World!");
+	System.out.println("Hello World!1");
 	System.out.println("2");
     }
 }
