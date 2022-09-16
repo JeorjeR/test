9c5d63020af269af28e3408b6488c57805b74d2b
Egor Roganin <eroganin@bk.ru>
2022-09-16 19:08:05 +0300

    delet some string

--- Hello.java
+++ Hello.java
@@ -2,10 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 	System.out.println("A");
-	System.out.println("1");
-	System.out.println("Hello World!2");
-	System.out.println("Hello World!1");
-	System.out.println("2");
+	System.out.println("Hello World!");
 	System.out.println("B");
     }
 }
